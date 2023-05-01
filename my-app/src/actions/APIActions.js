1be31948e18d44012d4e38904a0a398d24e18eeb
terror026 <developer.sahilb@gmail.com>
2023-05-01 12:56:38 +0530

    API enabled

--- my-app/src/actions/APIActions.js
+++ my-app/src/actions/APIActions.js
@@ -9,21 +9,21 @@ export const getInstagramUserIdByUsername = async (username) => {
             Toaster("error","Enter Username");
             return {};
         }
-        // const userDetails = await axios.get(
-		// 			`${baseURL}v1/user/by/username?username=` + username.trim(),
-		// 			{
-		// 				headers: {
-		// 					"x-access-key": API_KEY,
-		// 					"Content-Type": "application/json",
-		// 					"Access-Control-Allow-Origin": "*",
-		// 					"Access-Control-Allow-Headers": "GET, POST, OPTIONS",
-		// 					"Access-Control-Expose-Headers":
-		// 						"DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range",
-		// 				},
-		// 			},
-		// 		);
-        // return userDetails
-        return dummyUser;
+        const userDetails = await axios.get(
+					`${baseURL}v1/user/by/username?username=` + username.trim(),
+					{
+						headers: {
+							"x-access-key": API_KEY,
+							"Content-Type": "application/json",
+							"Access-Control-Allow-Origin": "*",
+							"Access-Control-Allow-Headers": "GET, POST, OPTIONS",
+							"Access-Control-Expose-Headers":
+								"DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range",
+						},
+					},
+				);
+        return userDetails
+        // return dummyUser;
     } catch (error) {
        console.log(error);
        Toaster("error",error);
@@ -31,33 +31,33 @@ export const getInstagramUserIdByUsername = async (username) => {
 };
 export const getUserFollowingByUserId = async (user,totalFollowing=200) =>{
    try{
-			// let isEnd_cursor=true;
-			// let UserFollowing = [];
-			// let URL = `${baseURL}/gql/user/following/chunk?user_id=${user?.pk}&amount=200&end_cursor=`;
-			// if(totalFollowing <= 200 ){
-			//     URL = `${baseURL}/v1/user/following/chunk?user_id=${user?.pk} `
-			// }
-			// let result = await callGetFollowingAPI(URL,user?.pk);
-			// const {details} = result || {};
-			// if(!details){
-			//     while(isEnd_cursor){
-			//         if(result[1].length > 5){
-			//             UserFollowing.push(result[0]);
-			//             URL = `${baseURL}/gql/user/following/chunk?user_id=${user?.pk}&amount=200&end_cursor=${result[1]}`;
-			//             result =  await callGetFollowingAPI(URL,user?.pk);
-			//         }else{
-			//             UserFollowing.push(result[0]);
-			//             isEnd_cursor= false;
-			//         }
-			//     }
-            // UserFollowing =UserFollowing.filter((item) => item.is_private == false)
+			let isEnd_cursor=true;
+			let UserFollowing = [];
+			let URL = `${baseURL}/gql/user/following/chunk?user_id=${user?.pk}&amount=200&end_cursor=`;
+			if(totalFollowing <= 200 ){
+			    URL = `${baseURL}/v1/user/following/chunk?user_id=${user?.pk} `
+			}
+			let result = await callGetFollowingAPI(URL,user?.pk);
+			const {details} = result || {};
+			if(!details){
+			    while(isEnd_cursor){
+			        if(result[1].length > 5){
+			            UserFollowing.push(result[0]);
+			            URL = `${baseURL}/gql/user/following/chunk?user_id=${user?.pk}&amount=200&end_cursor=${result[1]}`;
+			            result =  await callGetFollowingAPI(URL,user?.pk);
+			        }else{
+			            UserFollowing.push(result[0]);
+			            isEnd_cursor= false;
+			        }
+			    }
+            UserFollowing =UserFollowing.filter((item) => item.is_private == false)
 			// setUsersFollowing(UserFollowing);
-			// return UserFollowing;
+			return UserFollowing;
 
-			// }else{
-			//     return {};
-			// }
-			return dummyUserFollowing[0];
+			}else{
+			    return {};
+			}
+			// return dummyUserFollowing[0];
 		}catch(e){
         console.log(e);
    }
